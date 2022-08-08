26c17db10f41a3aaec550991b6e9e0919c9f54c5
pixel-punk-github <eomtaegyun08@gmail.com>
2022-08-08 15:46:14 +0900

    08-08-2022

---
+++ source/CreditsState.hx
@@ -0,0 +1,410 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+#if MODS_ALLOWED
+import sys.FileSystem;
+import sys.io.File;
+#end
+import lime.utils.Assets;
+
+using StringTools;
+
+class CreditsState extends MusicBeatState
+{
+	var curSelected:Int = -1;
+
+	private var grpOptions:FlxTypedGroup<Alphabet>;
+	// private var iconArray:Array<AttachedSprite> = [];
+	private var creditsStuff:Array<Array<String>> = [];
+
+	var bg:FlxSprite;
+	var descText:FlxText;
+	var intendedColor:Int;
+	var colorTween:FlxTween;
+	var descBox:AttachedSprite;
+
+	var offsetThing:Float = -75;
+
+	var arrow:FlxSprite;
+	var memberIcon:FlxSprite;
+	var teamName:FlxText;
+	var role:FlxText;
+	var memberName:FlxText;
+	var memberCharacter:FlxSprite;
+
+	var pressTimer:Int;
+
+	override function create()
+	{
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+
+		persistentUpdate = true;
+		bg = new FlxSprite().loadGraphic(Paths.image('creditBG'));
+		add(bg);
+		bg.screenCenter();
+
+		var creditBGCover:FlxSprite = new FlxSprite().loadGraphic(Paths.image('creditBGCover'));
+		add(creditBGCover);
+		creditBGCover.screenCenter();
+
+		arrow = new FlxSprite();
+		arrow.frames = Paths.getSparrowAtlas('creditArrow');
+		arrow.updateHitbox();
+		arrow.animation.addByPrefix('basic', 'basic');
+		arrow.animation.addByPrefix('start', 'start');
+		arrow.animation.addByPrefix('end', 'end');
+		arrow.animation.addByPrefix('up', 'UP');
+		arrow.animation.addByPrefix('down', 'DOWN');
+		arrow.animation.play('basic');
+		add(arrow);
+
+		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
+		
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		// add(grpOptions);
+
+		#if MODS_ALLOWED
+		var path:String = 'modsList.txt';
+		if(FileSystem.exists(path))
+		{
+			var leMods:Array<String> = CoolUtil.coolTextFile(path);
+			for (i in 0...leMods.length)
+			{
+				if(leMods.length > 1 && leMods[0].length > 0) {
+					var modSplit:Array<String> = leMods[i].split('|');
+					if(!Paths.ignoreModFolders.contains(modSplit[0].toLowerCase()) && !modsAdded.contains(modSplit[0]))
+					{
+						if(modSplit[1] == '1')
+							pushModCreditsToList(modSplit[0]);
+						else
+							modsAdded.push(modSplit[0]);
+					}
+				}
+			}
+		}
+
+		var arrayOfFolders:Array<String> = Paths.getModDirectories();
+		arrayOfFolders.push('');
+		for (folder in arrayOfFolders)
+		{
+			pushModCreditsToList(folder);
+		}
+		#end
+
+		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Chri_Dog',            'chriDog',          'Artist\nAnimator\nDirector',                                           '',                                     '',     'Phobos Mod',                 'chriDog'],
+			['Jipsa',               'jipsa',            'Character motion',                                                     '',                                     '',     'Phobos Mod',                 'jipsa'],
+			['Micky',               'micky',            'Artist\nAnimator\nComposer\nCutscene',                                 '',                                     '',     'Phobos Mod',                 'micky'],
+			['LK',                  'lk',               'Artist',                                                               '',                                     '',     'Phobos Mod',                 'lk'],
+			['Bamsquid',            'bamsquid',         'Artist\nIdea',                                                         '',                                     '',     'Phobos Mod',                 'bamsquid'],
+			['Grape',               'grape',            'Artist\nAnimator',                                                     '',                                     '',     'Phobos Mod',                 'grape'],
+			['Square Bracket',      'squareBracket',    'Programmer',                                                           '',                                     '',     'Phobos Mod',                 'squareBracket'],
+			['Shadow Mario',		'shadowmario',		'Main Programmer\nof Psych Engine',					            		'https://twitter.com/Shadow_Mario_',	'',     'Psych Engine Team',           null],
+			['RiverOaken',			'riveroaken',		'Main Artist/Animator\nof Psych Engine',			         			'https://twitter.com/RiverOaken',		'',     'Psych Engine Team',           null],
+			['shubs',				'shubs',			'Additional\nProgrammer\nof Psych Engine',			            		'https://twitter.com/yoshubs',			'',     'Psych Engine Team',           null],
+			['bb-panzu',			'bb-panzu',			'Ex-Programmer\nof Psych Engine',				            			'https://twitter.com/bbsub3',			'',     'Former Engine\nMembers',      null],
+			['iFlicky',				'iflicky',			'Composer\nof Psync and Tea Time\nMade the\nDialogue Sounds',       	'https://twitter.com/flicky_i',			'',     'Engine Contributors',         null],
+			['SqirraRNG',			'gedehari',			'Chart Editor\'s\nSound Waveform\nbase',			        			'https://twitter.com/gedehari',			'',     'Engine Contributors',         null],
+			['PolybiusProxy',		'polybiusproxy',	'.MP4 Video\nLoader Extension',						             		'https://twitter.com/polybiusproxy',	'',     'Engine Contributors',         null],
+			['Keoiki',				'keoiki',			'Note Splash\nAnimations',							             		'https://twitter.com/Keoiki_',			'',     'Engine Contributors',         null],
+			['Smokey',				'smokey',			'Spritemap\nTexture Support',					             			'https://twitter.com/Smokey_5_',		'',     'Engine Contributors',         null],
+			['ninjamuffin99',		'ninjamuffin99',	"Programmer\nof Friday Night Funkin'",			            			'https://twitter.com/ninja_muffin99',	'',     "Funkin' Crew",                null],
+			['PhantomArcade',		'phantomarcade',	"Animator\nof Friday Night Funkin'",					        		'https://twitter.com/PhantomArcade3K',	'',     "Funkin' Crew",                null],
+			['evilsk8r',			'evilsk8r',			"Artist\nof Friday Night Funkin'",						            	'https://twitter.com/evilsk8r',			'',     "Funkin' Crew",                null],
+			['kawaisprite',			'kawaisprite',		"Composer\nof Friday Night Funkin'",						        	'https://twitter.com/kawaisprite',		'',     "Funkin' Crew",                null]
+		];
+		
+		for(i in pisspoop){
+			creditsStuff.push(i);
+		}
+	
+		for (i in 0...creditsStuff.length)
+		{
+			var isSelectable:Bool = !unselectableCheck(i);
+			var optionText:Alphabet = new Alphabet(0, 70 * i, creditsStuff[i][0], !isSelectable, false);
+			optionText.isMenuItem = true;
+			optionText.screenCenter(X);
+			optionText.yAdd -= 70;
+			if(isSelectable) {
+				optionText.x -= 70;
+			}
+			optionText.forceX = optionText.x;
+			//optionText.yMult = 90;
+			optionText.targetY = i;
+			// grpOptions.add(optionText);
+
+			if(isSelectable) {
+				if(creditsStuff[i][5] != null)
+				{
+					Paths.currentModDirectory = creditsStuff[i][5];
+				}
+
+				// var icon:AttachedSprite = new AttachedSprite('credits/' + creditsStuff[i][1]);
+				// icon.xAdd = optionText.width + 10;
+				// icon.sprTracker = optionText;
+	
+				// // using a FlxGroup is too much fuss!
+				// iconArray.push(icon);
+				// add(icon);
+				Paths.currentModDirectory = '';
+
+				if(curSelected == -1) curSelected = i;
+			}
+		}
+
+		memberCharacter = new FlxSprite(850, 50);
+		memberCharacter.loadGraphic(Paths.image('credits/characters/Character-' + creditsStuff[0][6]));
+		add(memberCharacter);
+
+		memberIcon = new FlxSprite(130, 300);
+		memberIcon.loadGraphic(Paths.image('credits/' + creditsStuff[0][1]));
+		add(memberIcon);
+
+		teamName = new FlxText(-110, 100, FlxG.width, "", 20);
+		teamName.setFormat(Paths.font("impact.ttf"), 40, FlxColor.RED, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.WHITE);
+		teamName.text = creditsStuff[0][5];
+		add(teamName);
+
+		role = new FlxText(-110, 300, FlxG.width, "", 20);
+		role.setFormat(Paths.font("impact.ttf"), 40, FlxColor.WHITE, CENTER/*,FlxTextBorderStyle.OUTLINE, FlxColor.RED*/);
+		role.text = creditsStuff[0][2];
+		add(role);
+
+		memberName = new FlxText(375, 550, FlxG.width, "", 20);
+		memberName.setFormat(Paths.font("impact.ttf"), 50, FlxColor.WHITE, CENTER/*,FlxTextBorderStyle.OUTLINE, FlxColor.RED*/);
+		memberName.text = creditsStuff[0][0];
+		add(memberName);
+		
+		descBox = new AttachedSprite();
+		descBox.makeGraphic(1, 1, FlxColor.BLACK);
+		descBox.xAdd = -10;
+		descBox.yAdd = -10;
+		descBox.alphaMult = 0.6;
+		descBox.alpha = 0.6;
+		// add(descBox);
+
+		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
+		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER/*, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK*/);
+		descText.scrollFactor.set();
+		//descText.borderSize = 2.4;
+		descBox.sprTracker = descText;
+		// add(descText);
+
+		// bg.color = getCurrentBGColor();
+		// intendedColor = bg.color;
+		changeSelection();
+		super.create();
+	}
+
+	var quitting:Bool = false;
+	var holdTime:Float = 0;
+	override function update(elapsed:Float)
+	{
+		memberIcon.loadGraphic(Paths.image('credits/' + creditsStuff[curSelected][1]));
+		memberIcon.updateHitbox();
+
+		teamName.text = creditsStuff[curSelected][5];
+		role.text = creditsStuff[curSelected][2];
+		memberName.text = creditsStuff[curSelected][0];
+
+		//850, 50
+		if (creditsStuff[curSelected][6] == null) {
+			memberCharacter.loadGraphic(Paths.image('credits/' + creditsStuff[curSelected][1]));
+			memberCharacter.updateHitbox();
+			memberCharacter.x = 950;
+			memberCharacter.y = 160;
+		} else {
+			memberCharacter.x = 850;
+			memberCharacter.y = 50;
+			memberCharacter.loadGraphic(Paths.image('credits/characters/Character-' + creditsStuff[curSelected][6]));
+			memberCharacter.updateHitbox();
+		}
+
+		if (pressTimer < 0) {
+			if (curSelected == 0) arrow.animation.play('start');
+			else if (curSelected == creditsStuff.length - 1) arrow.animation.play('end');
+			else arrow.animation.play('basic');
+		}
+
+		if (FlxG.sound.music.volume < 0.7)
+		{
+			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+		}
+
+		if(!quitting)
+		{
+			if(creditsStuff.length > 1)
+			{
+				var shiftMult:Int = 1;
+				if(FlxG.keys.pressed.SHIFT) shiftMult = 3;
+
+				var upP = controls.UI_UP_P;
+				var downP = controls.UI_DOWN_P;
+
+				if (upP && curSelected != 0)
+				{
+					arrow.animation.play('up');
+					changeSelection(-1 * shiftMult);
+					holdTime = 0;
+				}
+				if (downP && curSelected != creditsStuff.length - 1)
+				{
+					arrow.animation.play('down');
+					changeSelection(1 * shiftMult);
+					holdTime = 0;
+				}
+
+				if (controls.UI_DOWN || controls.UI_UP)
+				{
+					pressTimer = 5;
+					var checkLastHold:Int = Math.floor((holdTime - 0.5) * 10);
+					holdTime += elapsed;
+					var checkNewHold:Int = Math.floor((holdTime - 0.5) * 10);
+
+					if (holdTime > 0.5 && checkNewHold - checkLastHold > 0)
+					{
+						changeSelection((checkNewHold - checkLastHold) * (controls.UI_UP ? -shiftMult : shiftMult));
+					}
+				}
+			}
+
+			if(controls.ACCEPT) {
+				CoolUtil.browserLoad(creditsStuff[curSelected][3]);
+			}
+			if (controls.BACK)
+			{
+				if(colorTween != null) {
+					colorTween.cancel();
+				}
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				MusicBeatState.switchState(new MainMenuState());
+				quitting = true;
+			}
+		}
+		
+		for (item in grpOptions.members)
+		{
+			if(!item.isBold)
+			{
+				var lerpVal:Float = CoolUtil.boundTo(elapsed * 12, 0, 1);
+				if(item.targetY == 0)
+				{
+					var lastX:Float = item.x;
+					item.screenCenter(X);
+					item.x = FlxMath.lerp(lastX, item.x - 70, lerpVal);
+					item.forceX = item.x;
+				}
+				else
+				{
+					item.x = FlxMath.lerp(item.x, 200 + -40 * Math.abs(item.targetY), lerpVal);
+					item.forceX = item.x;
+				}
+			}
+		}
+		pressTimer--;
+		super.update(elapsed);
+	}
+
+	var moveTween:FlxTween = null;
+	function changeSelection(change:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+		do {
+			curSelected += change;
+			if (curSelected < 0)
+				curSelected = creditsStuff.length - 1;
+			if (curSelected >= creditsStuff.length)
+				curSelected = 0;
+		} while(unselectableCheck(curSelected));
+
+		var newColor:Int =  getCurrentBGColor();
+		if(newColor != intendedColor) {
+			if(colorTween != null) {
+				colorTween.cancel();
+			}
+			intendedColor = newColor;
+			// colorTween = FlxTween.color(bg, 1, bg.color, intendedColor, {
+			// 	onComplete: function(twn:FlxTween) {
+			// 		colorTween = null;
+			// 	}
+			// });
+		}
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			if(!unselectableCheck(bullShit-1)) {
+				item.alpha = 0.6;
+				if (item.targetY == 0) {
+					item.alpha = 1;
+				}
+			}
+		}
+
+		descText.text = creditsStuff[curSelected][2];
+		descText.y = FlxG.height - descText.height + offsetThing - 60;
+
+		if(moveTween != null) moveTween.cancel();
+		moveTween = FlxTween.tween(descText, {y : descText.y + 75}, 0.25, {ease: FlxEase.sineOut});
+
+		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
+		descBox.updateHitbox();
+	}
+
+	#if MODS_ALLOWED
+	private var modsAdded:Array<String> = [];
+	function pushModCreditsToList(folder:String)
+	{
+		if(modsAdded.contains(folder)) return;
+
+		var creditsFile:String = null;
+		if(folder != null && folder.trim().length > 0) creditsFile = Paths.mods(folder + '/data/credits.txt');
+		else creditsFile = Paths.mods('data/credits.txt');
+
+		if (FileSystem.exists(creditsFile))
+		{
+			var firstarray:Array<String> = File.getContent(creditsFile).split('\n');
+			for(i in firstarray)
+			{
+				var arr:Array<String> = i.replace('\\n', '\n').split("::");
+				if(arr.length >= 5) arr.push(folder);
+				creditsStuff.push(arr);
+			}
+			creditsStuff.push(['']);
+		}
+		modsAdded.push(folder);
+	}
+	#end
+
+	function getCurrentBGColor() {
+		var bgColor:String = creditsStuff[curSelected][4];
+		if(!bgColor.startsWith('0x')) {
+			bgColor = '0xFF' + bgColor;
+		}
+		return Std.parseInt(bgColor);
+	}
+
+	private function unselectableCheck(num:Int):Bool {
+		return creditsStuff[num].length <= 1;
+	}
+}
---
+++ source/MainMenuState.hx
@@ -0,0 +1,339 @@
+package;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.effects.FlxFlicker;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import lime.app.Application;
+import Achievements;
+import editors.MasterEditorMenu;
+import flixel.input.keyboard.FlxKey;
+
+using StringTools;
+
+class MainMenuState extends MusicBeatState
+{
+	public static var psychEngineVersion:String = '0.5.2h'; //This is also used for Discord RPC
+	public static var curSelected:Int = 0;
+
+	var menuItems:FlxTypedGroup<FlxSprite>;
+	private var camGame:FlxCamera;
+	private var camAchievement:FlxCamera;
+	
+	var optionShit:Array<String> = [
+		'story_mode',
+		'freeplay',
+		'code_number',
+		// #if MODS_ALLOWED 'mods', #end
+		// #if ACHIEVEMENTS_ALLOWED 'awards', #end
+		'options',
+		'credits',
+		'music'
+		// #if !switch 'donate', #end
+	];
+
+	var camFollow:FlxObject;
+	var camFollowPos:FlxObject;
+	var debugKeys:Array<FlxKey>;
+
+	override function create()
+	{
+		WeekData.loadTheFirstEnabledMod();
+
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the Menus", null);
+		#end
+		debugKeys = ClientPrefs.copyKey(ClientPrefs.keyBinds.get('debug_1'));
+
+		camGame = new FlxCamera();
+		camAchievement = new FlxCamera();
+		camAchievement.bgColor.alpha = 0;
+
+		FlxG.cameras.reset(camGame);
+		FlxG.cameras.add(camAchievement);
+		FlxCamera.defaultCameras = [camGame];
+
+		transIn = FlxTransitionableState.defaultTransIn;
+		transOut = FlxTransitionableState.defaultTransOut;
+
+		persistentUpdate = persistentDraw = true;
+
+		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('redSky'));
+		bg.scrollFactor.set(0, 0);
+		bg.setGraphicSize(Std.int(bg.width * 1));
+		bg.updateHitbox();
+		bg.screenCenter();
+		bg.antialiasing = ClientPrefs.globalAntialiasing;
+		add(bg);
+
+		var buildings:FlxSprite = new FlxSprite(-80, -215).loadGraphic(Paths.image('buildings'));
+		buildings.scrollFactor.set(0, 0);
+		buildings.setGraphicSize(Std.int(buildings.width * 1.30)); //1.05
+		buildings.updateHitbox();
+		FlxTween.tween(buildings, { x: -80, y: 0 }, 25, { type: FlxTween.PINGPONG, ease: FlxEase.quadInOut, loopDelay: 2 });
+		buildings.antialiasing = ClientPrefs.globalAntialiasing;
+		add(buildings);
+
+		var titlephob:FlxSprite = new FlxSprite(-80);
+		titlephob.frames = Paths.getSparrowAtlas('titlephob');
+		titlephob.scrollFactor.set(0, 0);
+		titlephob.setGraphicSize(Std.int(titlephob.width * 1.05));
+		titlephob.updateHitbox();
+		titlephob.animation.addByPrefix('idle', 'Phobostitle 1', 24);
+		titlephob.animation.play('idle');
+		titlephob.x += 50;
+		titlephob.antialiasing = ClientPrefs.globalAntialiasing;
+		add(titlephob);
+
+		var redSquare:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('redSquare'));
+		redSquare.scrollFactor.set(0, 0);
+		redSquare.setGraphicSize(Std.int(redSquare.width * 1));
+		redSquare.updateHitbox();
+		redSquare.screenCenter(X);
+		redSquare.y = 230;
+		redSquare.antialiasing = ClientPrefs.globalAntialiasing;
+		add(redSquare);
+
+		var logoBl:FlxSprite = new FlxSprite(-80);
+		logoBl.frames = Paths.getSparrowAtlas('PhobosLogo');
+		logoBl.scrollFactor.set(0, 0);
+		logoBl.setGraphicSize(Std.int(logoBl.width * 0.6));
+		logoBl.antialiasing = ClientPrefs.globalAntialiasing;
+		logoBl.animation.addByPrefix('bump', 'Logo', 24, true);
+		logoBl.animation.play('bump');
+		logoBl.updateHitbox();
+		logoBl.screenCenter(X);
+		logoBl.y = 0;
+		add(logoBl);
+
+		var shadow:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('shadow'));
+		shadow.scrollFactor.set(0, 0);
+		shadow.setGraphicSize(Std.int(shadow.width * 1.05));
+		shadow.updateHitbox();
+		shadow.screenCenter();
+		shadow.antialiasing = ClientPrefs.globalAntialiasing;
+		add(shadow);
+
+		camFollow = new FlxObject(0, 0, 1, 1);
+		camFollowPos = new FlxObject(0, 0, 1, 1);
+		add(camFollow);
+		add(camFollowPos);
+		
+		// magenta.scrollFactor.set();
+
+		menuItems = new FlxTypedGroup<FlxSprite>();
+		add(menuItems);
+
+		var scale:Float = 1;
+		/*if(optionShit.length > 6) {
+			scale = 6 / optionShit.length;
+		}*/
+
+		for (i in 0...optionShit.length)
+		{
+			var offset:Float = 108 - (Math.max(optionShit.length, 4) - 4) * 80;
+			var menuItem:FlxSprite;
+			menuItem = new FlxSprite(0, 800); //(i * 60) + offset
+			menuItem.scale.x = scale * 1.25;
+			menuItem.scale.y = scale * 1.25;
+			// menuItem.y += 325;
+			menuItem.screenCenter(X);
+			FlxTween.tween(menuItem, { x: menuItem.x, y: i * 60 + 325 + offset }, 1, { ease: FlxEase.quintOut });
+			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
+			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
+			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
+			menuItem.animation.play('idle');
+			menuItem.ID = i;
+			menuItems.add(menuItem);
+			var scr:Float = (optionShit.length - 4) * 0.135;
+			if(optionShit.length < 6) scr = 0;
+			menuItem.scrollFactor.set(0, 0);
+			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
+			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
+			menuItem.updateHitbox();
+		}
+
+		FlxG.camera.follow(camFollowPos, null, 1);
+
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Psych Engine v" + psychEngineVersion, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
+
+		// NG.core.calls.event.logEvent('swag').send();
+
+		changeItem();
+
+		#if ACHIEVEMENTS_ALLOWED
+		Achievements.loadAchievements();
+		var leDate = Date.now();
+		if (leDate.getDay() == 5 && leDate.getHours() >= 18) {
+			var achieveID:Int = Achievements.getAchievementIndex('friday_night_play');
+			if(!Achievements.isAchievementUnlocked(Achievements.achievementsStuff[achieveID][2])) { //It's a friday night. WEEEEEEEEEEEEEEEEEE
+				Achievements.achievementsMap.set(Achievements.achievementsStuff[achieveID][2], true);
+				giveAchievement();
+				ClientPrefs.saveSettings();
+			}
+		}
+		#end
+
+		super.create();
+	}
+
+	#if ACHIEVEMENTS_ALLOWED
+	// Unlocks "Freaky on a Friday Night" achievement
+	function giveAchievement() {
+		add(new AchievementObject('friday_night_play', camAchievement));
+		FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+		trace('Giving achievement "friday_night_play"');
+	}
+	#end
+
+	var selectedSomethin:Bool = false;
+
+	override function update(elapsed:Float)
+	{
+		if (FlxG.sound.music.volume < 0.8)
+		{
+			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+		}
+
+		var lerpVal:Float = CoolUtil.boundTo(elapsed * 7.5, 0, 1);
+		camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+
+		if (!selectedSomethin)
+		{
+			if (controls.UI_UP_P)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				changeItem(-1);
+			}
+
+			if (controls.UI_DOWN_P)
+			{
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				changeItem(1);
+			}
+
+			if (controls.BACK)
+			{
+				selectedSomethin = true;
+				FlxG.sound.play(Paths.sound('cancelMenu'));
+				MusicBeatState.switchState(new TitleState());
+			}
+
+			if (controls.ACCEPT)
+			{
+				if (optionShit[curSelected] == 'donate')
+				{
+					CoolUtil.browserLoad('https://ninja-muffin24.itch.io/funkin');
+				}
+				else
+				{
+					selectedSomethin = true;
+					FlxG.sound.play(Paths.sound('confirmMenu2'));
+
+					// if(ClientPrefs.flashing) FlxFlicker.flicker(magenta, 1.1, 0.15, false);
+
+					menuItems.forEach(function(spr:FlxSprite)
+					{
+						if (curSelected != spr.ID)
+						{
+							FlxTween.tween(spr, {alpha: 0}, 0.4, {
+								ease: FlxEase.quadOut,
+								onComplete: function(twn:FlxTween)
+								{
+									spr.kill();
+								}
+							});
+						}
+						else
+						{
+							FlxFlicker.flicker(spr, 1, 0.06, false, false, function(flick:FlxFlicker)
+							{
+								var daChoice:String = optionShit[curSelected];
+
+								switch (daChoice)
+								{
+									case 'story_mode':
+										MusicBeatState.switchState(new StoryMenuState());
+									case 'freeplay':
+										MusicBeatState.switchState(new FreeplayState());
+									#if MODS_ALLOWED
+									case 'mods':
+										MusicBeatState.switchState(new ModsMenuState());
+									#end
+									case 'awards':
+										MusicBeatState.switchState(new AchievementsMenuState());
+									case 'credits':
+										MusicBeatState.switchState(new CreditsState());
+									case 'options':
+										LoadingState.loadAndSwitchState(new options.OptionsState());
+								}
+							});
+						}
+					});
+				}
+			}
+			#if desktop
+			else if (FlxG.keys.anyJustPressed(debugKeys))
+			{
+				selectedSomethin = true;
+				MusicBeatState.switchState(new MasterEditorMenu());
+			}
+			#end
+		}
+
+		super.update(elapsed);
+
+		menuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.screenCenter(X);
+		});
+	}
+
+	function changeItem(huh:Int = 0)
+	{
+		curSelected += huh;
+
+		if (curSelected >= menuItems.length)
+			curSelected = 0;
+		if (curSelected < 0)
+			curSelected = menuItems.length - 1;
+
+		menuItems.forEach(function(spr:FlxSprite)
+		{
+			spr.animation.play('idle');
+			spr.updateHitbox();
+
+			if (spr.ID == curSelected)
+			{
+				spr.animation.play('selected');
+				var add:Float = 0;
+				if(menuItems.length > 4) {
+					add = menuItems.length * 8;
+				}
+				camFollow.setPosition(spr.getGraphicMidpoint().x, spr.getGraphicMidpoint().y - add);
+				spr.centerOffsets();
+			}
+		});
+	}
+}
